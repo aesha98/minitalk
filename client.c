d88c60601e6b6ae6579560eda73b0bf6ef7c2aee
aesha98 <sityasya@gmail.com>
2022-03-29 16:41:06 +0800

    update

---
+++ client.c
@@ -0,0 +1,14 @@
+#include "minitalk.h"
+
+static char *message;
+
+static void signal_handler()
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+
+    return (0);
+}
--- server.c
+++ server.c
@@ -1,6 +1,15 @@
 #include "minitalk.h"
 
+static void signal_handler(int sig, siginfo_t *info, void *context)
+
+
 int main(void)
 {
+    pid_t pid;
+    struct sigaction signal;
+
+    signal.sa_sigaction = &signal_handler;
     
+    return (0);
+
 }
